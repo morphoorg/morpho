0e03dd2eb29b9fbf44c45fe91dcbefd986fde153
guiguem <guig098@we30935.pnl.gov>
2016-02-23 10:53:36 -0800

    get architecture from Talia branch

---
+++ examples/tritium_model/models/tritium_analysis.stan
@@ -0,0 +1,259 @@
+/*
+* Tritium Spectrum Model (Analysis)
+* -----------------------------------------------------
+* Copyright: J. A. Formaggio <josephf@mit.edu>
+*
+* Date: 25 March 2015
+* Modified: Feb 17 2016 by MG
+*
+* Purpose:
+*
+*		Program will analyze a set of generated data distributed according to the beta decay spectrum of tritium.
+*		Program assumes Project-8 measurement (frequency) and molecular tritium as the source.
+*		Spectrum includes simplified beta-function with final state distribution (assuming 0.36 eV gaussian model)
+*		Includes broadening due to magnetic field homogeneity, radiative energy loss, Doppler and scattering.
+*		T_2 scattering cross-section model implemented.
+*		Note that sample events are distributed according to true distribution.
+*
+*
+* Collaboration:  Project 8
+*/
+
+data {
+
+  //   Number of neutrinos and mixing parameters
+  //if MassHierarchy = 1, normal hierarchy (delta_m31>0)
+  //if MassHierarchy = -1, inverted hierarchy (delta_m31<0)
+  int MassHierarchy;
+  real neutrino_mass_limit;
+
+  int nFamily;
+  real meas_delta_m21;
+  real meas_delta_m32_NH;
+  real meas_delta_m32_IH;
+
+  real meas_sin2_th12;
+  real meas_sin2_th13_NH;
+  real meas_sin2_th13_IH;
+
+  real meas_delta_m21_err;
+  real meas_delta_m32_NH_err;
+  real meas_delta_m32_IH_err;
+
+  real meas_sin2_th12_err;
+  real meas_sin2_th13_NH_err;
+  real meas_sin2_th13_IH_err;
+
+  //   Primary magnetic field (in Tesla)
+
+  real<lower=0> BField;
+  real BFieldError;
+
+  //   Range for fits (in Hz)
+
+  real<lower=0 > minKE;
+  real<lower=minKE> maxKE;
+
+  //   Endpoint of tritium, in eV
+
+  real QValue;
+  real QValue_Error;
+  real sigmaQ;
+
+  //  Conditions of the experiment and measurement
+
+  real number_density;				//  Tritium number density (in meter^-3)
+  real temperature;				//  Temperature of gas (in Kelvin)
+  real effective_volume;		        //  Effective volume (in meter^3)
+  real measuring_time;			//  Measuring time (in seconds)
+
+  //  Clock and filter information
+
+  real fBandpass;
+  real fclockError;
+  int  fFilterN;
+
+  //  Input data from generated sample
+  //
+  int nBinTime;
+  vector[nBinTime] time_data;
+  int n_time_data[nBinTime];
+  int nBinEvents;
+  vector[nBinEvents] freq_data;
+  int n_events[nBinEvents];
+
+}
+
+transformed data {
+
+  real minFreq;
+  real maxFreq;
+  real fclock;
+
+  minFreq <- get_frequency(maxKE, BField);
+  maxFreq <- get_frequency(minKE, BField);
+
+  fclock <- 0.;
+
+  if (fBandpass > (maxFreq-minFreq)) {
+    print("Bandpass filter (",fBandpass,") is greater than energy range (",maxFreq-minFreq,").");
+    print("Consider enlarging energy region.");
+  }
+
+  print("Approximate activity = ",tritium_rate_per_eV() * beta_integral(QValue, 0., minKE) * number_density * effective_volume / tritium_halflife() / log(2.) * measuring_time);
+
+  print("Approximate rate (Hz) = ",tritium_rate_per_eV() * beta_integral(QValue, 0., minKE) * number_density * effective_volume / tritium_halflife() / log(2.));
+
+}
+
+parameters {
+
+  real<lower=0.,upper=1.> signal_fraction;
+  real<lower=0.5, upper=1.5> mu_tot;
+  real<lower=0.,upper=4.> lightest_neutrino_mass;
+
+  real uB;
+  real uF;
+  real uQ0;
+  real uQ;
+
+
+  real udm21;
+  real udm32;
+  real us12;
+  real us13;
+  real<lower=0.0> eDop;
+  real<lower=3.5e4,upper=10e4> scatt_width;
+  real<lower=10000, upper=30000> n0_timeData;
+}
+
+transformed parameters{
+
+  real KE;
+  real frequency;
+
+  // real signal_rate;
+  real<lower=0> MainField;
+  real Q0;
+  real Q;
+  real df;
+  real rad_width;
+  real tot_width;
+  real sigma_freq;
+
+  real kDoppler;
+  real KE_shift;
+
+  real activity;
+  real rate_log;
+  real total_rate;
+
+  vector[nBinEvents] rate;
+
+  real dm21;
+  real dm32;
+  real dm31;
+
+  real s12;
+  real s13;
+
+  vector[nFamily] m_nu;
+  vector[nFamily] U_PMNS;
+  real<lower = 0.> neutrino_mass;
+
+    // Priors on the neutrino mixin parameters and mass differences
+
+  dm21 <- vnormal_lp(udm21,meas_delta_m21,meas_delta_m21_err);
+  s12 <- fabs(vnormal_lp(us12, meas_sin2_th12,meas_sin2_th12_err));
+
+  if (MassHierarchy == 1)
+  {
+    dm32 <- vnormal_lp(udm32, meas_delta_m32_NH,meas_delta_m32_NH_err);
+    s13 <- fabs(vnormal_lp(us13,meas_sin2_th13_NH,meas_sin2_th13_NH_err));
+    dm31 <- meas_delta_m32_NH + meas_delta_m21;
+    m_nu[1] <- lightest_neutrino_mass;
+    m_nu[2] <- sqrt(fabs(dm21 + square(m_nu[1])));
+    m_nu[3] <- sqrt(fabs(dm31 + square(m_nu[1])));
+  }
+  else if (MassHierarchy == -1)
+  {
+    dm32 <- vnormal_lp(udm32,meas_delta_m32_IH,meas_delta_m32_IH_err);
+    s13 <- fabs(vnormal_lp(us13,meas_sin2_th13_IH,meas_sin2_th13_IH_err));
+    dm31 <- meas_delta_m32_IH + meas_delta_m21;
+    m_nu[3] <- lightest_neutrino_mass;
+    m_nu[1] <- sqrt(fabs(-dm31 + square(m_nu[3])));
+    m_nu[2] <- sqrt(fabs(-dm32 + square(m_nu[3])));
+  }
+  U_PMNS <- get_U_PMNS(nFamily,fabs(s12),fabs(s13));
+
+  neutrino_mass <- fabs(dot_product(U_PMNS,m_nu));
+
+  //   Obtain magnetic field (with prior distribution)
+
+  MainField <- vnormal_lp(uB, BField, BFieldError);
+
+  //   Calculate scattering length, radiation width, and total width;
+
+  rad_width <- cyclotron_rad(MainField);
+  tot_width <- (scatt_width + rad_width);
+  sigma_freq <- (scatt_width + rad_width) / (4. * pi());
+
+  #  Determining endpoint
+  # Here is where the model from Talia should be added
+
+  Q0 <- vnormal_lp(uQ0, QValue, QValue_Error);
+  Q <- vnormal_lp(uQ, Q0, sigmaQ);
+
+  // Determine total rate from activity
+  activity <- tritium_rate_per_eV() * beta_integral(Q, neutrino_mass, minKE) * number_density * effective_volume / (tritium_halflife() / log(2.) );
+  total_rate <- mu_tot * activity * measuring_time;
+
+  df <- vnormal_lp(uF, 0.0, sigma_freq);
+  for (i in 1:nBinEvents){
+    frequency <- freq_data[i] - df + fclock;
+
+    KE <- get_kinetic_energy(frequency, MainField);
+
+    kDoppler <-  m_electron() * get_velocity(QValue) * sqrt(eDop / tritium_molecular_mass());
+
+    KE_shift <- KE - kDoppler;
+
+    //   Determine signal and background rates from beta function and background level
+    rate_log <- signal_to_noise_log(KE_shift, Q, U_PMNS, m_nu , minKE, maxKE, signal_fraction);
+    rate[i] <- total_rate * exp(rate_log);
+  }
+
+  // Print the parameters of interest (for debug only...)
+  // print(lightest_neutrino_mass, "  ", signal_fraction, "  ", mu_tot, "  ", eDop, "  ",scatt_width, "  ", n0_timeData);
+
+}
+
+model {
+
+  #  Thermal Doppler broadening of the tritium source
+
+  eDop ~ gamma(1.5,1./(k_boltzmann() * temperature));
+
+  for (i in 1:nBinTime)
+  {
+    if(n_time_data[i]!=0)
+    {
+      n_time_data[i] ~ normal( n0_timeData * exp( - time_data[i] * scatt_width ) , sqrt( n_time_data[i]*1. ) );
+    }
+  }
+
+  mu_tot ~ uniform(0.5 ,1.5);
+  for (i in 1:nBinEvents)
+  {
+    if(rate[i]!=0)
+    {
+      n_events[i] ~ poisson(rate[i]);
+    }
+  }
+
+
+}
+
+generated quantities {
+
+}
---
+++ morpho/morpho.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+
+#
+# morpho.py
+#
+# author: j.n. kofron <jared.kofron@gmail.com>
+#
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+import re
+import pystan
+import pystanLoad as pyL
+import json
+import fileinput
+
+from pystan import stan
+from h5py import File as HDF5
+from yaml import load as yload
+from argparse import ArgumentParser
+from inspect import getargspec
+
+import pickle
+from hashlib import md5
+
+class stan_args(object):
+    def read_param(self, yaml_data, node, default):
+        data = yaml_data
+        xpath = node.split('.')
+        try:
+            for path in xpath:
+                data = data[path]
+        except Exception as exc:
+            if default == 'required':
+                err = """FATAL: Configuration parameter {0} required but not\
+                provided in config file!
+                """.format(node)
+                print(err)
+                raise exc
+            else:
+                data = default
+        return data
+
+    def gen_arg_dict(self):
+        d = self.__dict__
+        sca = getargspec(stan_cache)
+        sa = getargspec(stan)
+        return {k: d[k] for k in (sa.args + sca.args) if k in d}
+
+    def init_function(self):
+        return self.init_per_chain
+    
+    def __init__(self, yd):
+        try:
+            # Identifications
+            self.job_id = self.read_param(yd, 'stan.job_id', '0')
+
+            # STAN model stuff
+            self.model_code = self.read_param(yd, 'stan.model.file', 'required')
+            self.functions_code = self.read_param(yd, 'stan.model.function_file', None)
+            self.cashe_dir = self.read_param(yd, 'stan.model.cache', './cache')
+
+            # STAN data
+            datafiles = self.read_param(yd, 'stan.data', 'required')
+            self.data = pyL.stan_data_files(datafiles)
+
+            # STAN run conditions
+            self.algorithm = self.read_param(yd, 'stan.run.algorithm', 'NUTS')
+            self.iter = self.read_param(yd, 'stan.run.iter', 2000)
+            self.warmup = self.read_param(yd, 'stan.run.warmup', self.iter/2)
+            self.chains = self.read_param(yd, 'stan.run.chain', 4)
+            self.seed = self.read_param(yd, 'stan.run.seed', 314159)
+            self.thin = self.read_param(yd, 'stan.run.thin', 1)
+            self.init_per_chain = self.read_param(yd, 'stan.run.init', '')
+            self.init = self.init_function();
+                        
+            # plot and print information
+            self.plot_vars = self.read_param(yd, 'stan.plot', None)
+
+            # output information
+            self.out_format = self.read_param(yd, 'stan.output.format', 'hdf5')
+            self.out_fname = self.read_param(yd, 'stan.output.name',
+                                                 'stan_out.h5')
+
+            self.out_tree = self.read_param(yd, 'stan.output.tree', None)
+            self.out_branches = self.read_param(yd, 'stan.output.branches', None)
+
+            self.out_cfg = self.read_param(yd, 'stan.output.config', None)
+            self.out_vars = self.read_param(yd, 'stan.output.data', None)
+            
+            # Outputted pickled fit filename
+            self.out_fit = self.read_param(yd, 'stan.output.fit', None)
+            
+        except Exception as err:
+            raise err
+
+def stan_cache(model_code, functions_code, model_name=None, cashe_dir='.',**kwargs):
+    """Use just as you would `stan`"""
+
+    theData = open(model_code,'r+').read()
+    code_hash = md5(theData.encode('ascii')).hexdigest()
+    if model_name is None:
+        cache_fn = '{}/cached-model-{}.pkl'.format(cashe_dir, code_hash)
+    else:
+        cache_fn = '{}/cached-{}-{}.pkl'.format(cashe_dir, model_name, code_hash)
+    try:
+        sm = pickle.load(open(cache_fn, 'rb'))
+    except:
+        theModel = theData
+        if functions_code:
+            match = re.findall(r"(?<=include_functions<-)\w+",theData, flags=re.IGNORECASE)
+            if match:
+                for matches in match:
+                    for key in functions_code:
+                        if (key['name']==matches):
+                            StanFunctions = open(key['file'],'r+').read()
+                            theModel = re.sub("include_functions<-"+matches, StanFunctions, theModel, flags=re.IGNORECASE)
+        sm = pystan.StanModel(model_code=theModel)
+        with open(cache_fn, 'wb') as f:
+            pickle.dump(sm, f)
+    else:
+        print("Using cached StanModel")
+    return sm.sampling(**kwargs)
+
+def parse_args():
+    '''
+    Parse the command line arguments provided to morpho.
+    '''
+    p = ArgumentParser(description='''
+        An analysis tool for Project 8 data.
+    ''')
+    p.add_argument('--config',
+                   metavar='<configuration file>',
+                   help='Full path to the configuration file used by morpho',
+                   required=True)
+    p.add_argument('--job_id',
+                   metavar='<job_id>',
+                   help='Job id number for batching',
+                   required=False)
+    p.add_argument('--seed',
+                   metavar='<seed>',
+                   help='Add random seed number to file',
+                   required=False)                   
+
+    return p.parse_args()
+
+
+def open_or_create(hdf5obj, groupname):
+    """
+    Create a group within an hdf5 object if it doesn't already exist,
+    and return the resulting group.
+    """
+    if groupname != "/" and groupname not in hdf5obj.keys():
+        hdf5obj.create_group(groupname)
+    return hdf5obj[groupname]
+
+def plot_result(conf, stanres):
+    """
+    Plot variables as specified.
+    """
+    fit = stanres.extract()
+    if conf.plot_vars is not None:
+        for var in conf.plot_vars:
+            parname = var['variable']
+            if parname not in fit:
+                warning = """WARNING: data {0} not found in fit!  Skipping...
+                """.format(parname)
+                print warning
+            else:
+                stanres.plot(parname)
+        print(result)
+
+def write_result(conf, stanres):
+
+    ofilename = sa.out_fname
+    if (args.job_id>0):
+        ofilename = ofilename+'_'+args.job_id        
+    if sa.out_format == 'hdf5':
+        #ofilename = ofilename+'.h5'
+        write_result_hdf5(sa, ofilename, result)
+
+    if sa.out_format == 'root':
+        ofilename = ofilename+'.root'
+        pyL.stan_write_root(sa, ofilename, result)
+    return stanres
+
+def write_result_hdf5(conf, ofilename, stanres):
+    """
+    Write the STAN result to an HDF5 file.
+    """
+    with HDF5(ofilename,'w') as ofile:
+        g = open_or_create(ofile, conf.out_cfg['group'])
+        fit = stanres.extract()
+        for var in conf.out_vars:
+            stan_parname = var['stan_parameter']
+            if stan_parname not in fit:
+                warning = """WARNING: data {0} not found in fit!  Skipping...
+                """.format(stan_parname)
+                print warning
+            else:
+                print(var['output_name'])
+                g[var['output_name']] = fit[stan_parname]
+                
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+if __name__ == '__main__':
+    args = parse_args()
+    with open(args.config, 'r') as cfile:
+        try:
+            cdata = yload(cfile)
+            sa = stan_args(cdata)
+
+            result = stan_cache(**(sa.gen_arg_dict()))
+
+            stanres = write_result(sa, result)
+            plot_result(sa, result)
+        
+            save_object(stanres, sa.out_fit)
+                                        
+        except Exception as err:
+            print(err)

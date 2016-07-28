2a38543729ec6f1e278b63582da749d95b216ae9
guiguem <mathieu.guigue@pnnl.gov>
2016-07-27 17:13:01 -0700

    The first steps are the hardest...

---
+++ morpho/plot/histo.py
@@ -0,0 +1,8 @@
+import ROOT as ROOT# import ROOT, TStyle, TCanvas, TH1F, TGraph, TLatex, TLegend, TFile, TTree, TGaxis, TRandom3, TNtuple, TTree
+import cmath as math
+from array import array
+import re
+
+def histo(param_dict):
+    print("Creating a histo!")
+    

febfe992d99a0152aba52eb303ebc157af0c91f6
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2016-11-04 16:08:21 -0700

    Preliminary timeseries plotting function

---
+++ morpho/plot/timeseries.py
@@ -0,0 +1,204 @@
+#======================================================
+# histo.py
+#
+# Author: M. Guigue, T. E. Weiss
+# Date: Aug. 4, 2016
+#
+# Description:
+#
+# Generic methods to display histograms with ROOT
+#=======================================================
+
+"""
+To do:
+    - Clean up
+    - Add log
+"""
+
+import ROOT as ROOT
+import cmath as math
+from array import array
+import re
+import uuid
+
+def set_style_options( rightMargin,  leftMargin,  topMargin,  botMargin):
+    style = ROOT.TStyle(ROOT.gStyle)
+    style.SetOptStat("emr")
+    style.SetLabelOffset(0.01,'xy')
+    style.SetLabelSize(0.05,'xy')
+    style.SetTitleOffset(1.2,'y')
+    style.SetTitleSize(0.05,'x')
+    style.SetTitleSize(0.05,'y')
+    # style.SetLabelSize(0.05,'y')
+    # style.SetLabelOffset(0,'y')
+    # style.SetLabelSize(0.05,'x')
+    style.SetTitleOffset(1.02,'x')
+
+    style.SetPadRightMargin(rightMargin)
+    style.SetPadTopMargin(topMargin)
+    style.SetPadBottomMargin(botMargin)
+    style.SetPadLeftMargin(leftMargin)
+    style.cd()
+
+def timeseries(param_dict):
+
+    # Preparing the canvas
+    if 'title' in param_dict and param_dict['title']!='':
+        title = param_dict['title']
+        set_style_options(0.04,0.1,0.07,0.12)
+
+    else:
+        title = ' ' #canvas_'+uuid.uuid4().get_hex()
+        set_style_options(0.04,0.1,0.03,0.12)
+
+    if 'output_width' in param_dict:
+        width = param_dict['output_width']
+    else:
+        width = 600
+    if 'output_height' in param_dict:
+        height = param_dict['output_height']
+    else:
+        height = 400
+    can = ROOT.TCanvas(title,title,width,height)
+    ntimeseries = len(param_dict['data'])
+    nCanx = int(ROOT.TMath.Sqrt(ntimeseries) )+1
+    nCany = int(ROOT.TMath.Sqrt(ntimeseries) )+1
+    can.Divide(nCanx,nCany)
+    # Setting the titles
+    if 'x_title' in param_dict:
+        xtitle = param_dict['x_title']
+    else:
+        xtitle = ['']*ntimeseries
+    print(xtitle)
+    if 'y_title' in param_dict:
+        ytitle = param_dict['y_title']
+    else:
+        ytitle = ['']*ntimeseries
+    print(ytitle)
+
+    if 'draw_opt' in param_dict:
+        drawing_options = param_dict['draw_opt']
+    else:
+        drawing_options = "APL"
+    # if 'axes_opt' in param_dict:
+    #     axes_options = param_dict['axes_opt']
+    # else:
+    #     axes_options = ''
+
+    lgraph = {}
+
+    myfile = ROOT.TFile(param_dict['input_file_name'],"READ")
+    tree = myfile.Get(param_dict['input_tree'])
+    n = tree.GetEntries()
+    print(n)
+    iCanvas = 0
+    for dataname in param_dict['data']:
+        print(dataname)
+        list_datax = [0.]*n
+        list_datay = [0.]*n
+        for i in range(0,n):
+            tree.GetEntry(i)
+            list_datax[i] = float(i)
+            list_datay[i] = getattr(tree,dataname)
+        lgraph[dataname] = ROOT.TGraph(n,array('d',list_datax),array('d',list_datay))
+        can.cd(iCanvas+1)
+        lgraph[dataname].Draw(drawing_options)
+        lgraph[dataname].GetXaxis().SetTitle(xtitle[iCanvas])
+        lgraph[dataname].GetYaxis().SetTitle(ytitle[iCanvas])
+        iCanvas +=1
+    can.Update()
+
+    # gSave.append(list_histo)
+
+    # Setting the picture file name
+    if 'output_path' in param_dict:
+        path = param_dict['output_path']
+    else:
+        path = "./"
+    if path.endswith('/')==False:
+        path = path + '/'
+    if title!=' ':
+        figurefullpath = path+title+'_'
+    else:
+        figurefullpath = path
+    for namedata in param_dict['data']:
+        figurefullpath += namedata + '_'
+    if 'output_format' in param_dict:
+        figurefullpath += param_dict['output_format']
+    else:
+        figurefullpath += '.pdf'
+    can.SaveAs(figurefullpath)
+    raw_input('Press <ret> to end -> ')
+
+    return can
+
+
+# can = TCanvas("can", "can", 200, 10, 600, 400)
+# #    Setting x axis title
+# if isinstance(title[0], basestring) == False:
+#     print 'Error: x axis title is not a string: ' + title[0]
+#     xtitle = "x"
+# else:
+#     xtitle = title[0]
+# #    Setting y axis title
+# if isinstance(title[1], basestring) == False:
+#     print 'Error: y axis title is not a string: ' + title[1]
+#     ytitle = " "
+# else:
+#     ytitle = title[1]
+#
+# if isinstance(title[2], basestring) == False:
+#     print 'Error: pdfname is not a string: ' + title[2]
+#     pdfname = "default"
+# else:
+#     pdfname = title[2]
+#
+# #    Checking if option format is OK
+# if len(option) != 3:
+#     print 'Error: Number of options != 3: (%d)' % len(option)
+#     return
+# else :
+#     if option[2][0] < option[2][1]:
+#         xmin = option[2][0]
+#         xmax = option[2][1]
+#     else:
+#         print 'Error: Invalid range of histogram -> Setting automatic range!'
+#         xmin, xmax = autoRange(list)
+#         print ' Range is: %d, %d' % (xmin, xmax)
+#
+# if option[1] >= 2:
+#     nbins = option[1]
+#     print nbins
+# else:
+#     print 'Error: invalid number of bins -> Setting nbin = 100'
+#     nbins = 100
+#
+# if isinstance(option[0], basestring) == False:
+#         print 'Error: first argument of option is not a string' + option[0]
+#         return
+#
+#
+#
+#
+# if "logx" in option[0]:
+#     if xmin > 0:
+#         can.SetLogx()
+#
+#         h1 = fillLogxHisto(list, xmin, xmax, nbins)
+#     else:
+#         print 'Error: xmin negative or null -> Cannot have logx axis!'
+#         return
+# else:
+#     h1 = TH1F("h1", "", nbins, xmin, xmax)
+#     for i in range(0, len(list)):
+#         h1.Fill(list[i])
+#
+# if "logy" in option[0]:
+#     can.SetLogy()
+#
+# can.cd()
+# h1.GetXaxis().SetTitle(xtitle)
+# h1.GetYaxis().SetTitle(ytitle)
+# h1.Draw()
+# can.SaveAs("plots/" + pdfname + ".pdf")
+# return can

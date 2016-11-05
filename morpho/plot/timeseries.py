1f9716973c0309c96579475611410bd05c2bb581
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2016-11-04 17:22:10 -0700

    Various cleaning

--- morpho/plot/timeseries.py
+++ morpho/plot/timeseries.py
@@ -12,7 +12,6 @@
 """
 To do:
     - Clean up
-    - Add log
 """
 
 import ROOT as ROOT
@@ -80,10 +79,6 @@ def timeseries(param_dict):
         drawing_options = param_dict['draw_opt']
     else:
         drawing_options = "APL"
-    # if 'axes_opt' in param_dict:
-    #     axes_options = param_dict['axes_opt']
-    # else:
-    #     axes_options = ''
 
     lgraph = {}
 
@@ -131,74 +126,3 @@ def timeseries(param_dict):
     raw_input('Press <ret> to end -> ')
 
     return can
-
-
-# can = TCanvas("can", "can", 200, 10, 600, 400)
-# #    Setting x axis title
-# if isinstance(title[0], basestring) == False:
-#     print 'Error: x axis title is not a string: ' + title[0]
-#     xtitle = "x"
-# else:
-#     xtitle = title[0]
-# #    Setting y axis title
-# if isinstance(title[1], basestring) == False:
-#     print 'Error: y axis title is not a string: ' + title[1]
-#     ytitle = " "
-# else:
-#     ytitle = title[1]
-#
-# if isinstance(title[2], basestring) == False:
-#     print 'Error: pdfname is not a string: ' + title[2]
-#     pdfname = "default"
-# else:
-#     pdfname = title[2]
-#
-# #    Checking if option format is OK
-# if len(option) != 3:
-#     print 'Error: Number of options != 3: (%d)' % len(option)
-#     return
-# else :
-#     if option[2][0] < option[2][1]:
-#         xmin = option[2][0]
-#         xmax = option[2][1]
-#     else:
-#         print 'Error: Invalid range of histogram -> Setting automatic range!'
-#         xmin, xmax = autoRange(list)
-#         print ' Range is: %d, %d' % (xmin, xmax)
-#
-# if option[1] >= 2:
-#     nbins = option[1]
-#     print nbins
-# else:
-#     print 'Error: invalid number of bins -> Setting nbin = 100'
-#     nbins = 100
-#
-# if isinstance(option[0], basestring) == False:
-#         print 'Error: first argument of option is not a string' + option[0]
-#         return
-#
-#
-#
-#
-# if "logx" in option[0]:
-#     if xmin > 0:
-#         can.SetLogx()
-#
-#         h1 = fillLogxHisto(list, xmin, xmax, nbins)
-#     else:
-#         print 'Error: xmin negative or null -> Cannot have logx axis!'
-#         return
-# else:
-#     h1 = TH1F("h1", "", nbins, xmin, xmax)
-#     for i in range(0, len(list)):
-#         h1.Fill(list[i])
-#
-# if "logy" in option[0]:
-#     can.SetLogy()
-#
-# can.cd()
-# h1.GetXaxis().SetTitle(xtitle)
-# h1.GetYaxis().SetTitle(ytitle)
-# h1.Draw()
-# can.SaveAs("plots/" + pdfname + ".pdf")
-# return can

377ef53c2a67f417be33bf1d7e249cf470968095
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2016-12-12 22:17:51 -0800

    Correctly install morpho inside a virtual environment, morpho then get accessible via morpho instead of python ../morpho/morpho.py

--- morpho/__init__.py
+++ morpho/__init__.py
@@ -6,3 +6,7 @@ from __future__ import absolute_import
 import pkg_resources
 __version__ = pkg_resources.require("morpho")[0].version.split('-')[0]
 __commit__ = pkg_resources.require("morpho")[0].version.split('-')[-1]
+
+from . import plot
+from . import postprocessing
+from . import loader

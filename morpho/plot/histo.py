913d45998fc7ecce3fd58105cfc978e8af713eef
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2016-12-13 10:25:43 -0800

    Moving things to tritium_model folder

--- morpho/plot/histo.py
+++ morpho/plot/histo.py
@@ -189,6 +189,125 @@ def histo(param_dict):
 
     return can
 
+
+def spectra(param_dict):
+
+    # Preparing the canvas
+    logger.debug("Preparing Canvas")
+    title, width, height = preparingCanvas(param_dict)
+    can = ROOT.TCanvas(title,title,width,height)
+
+    # Setting the titles
+    logger.debug("Preparing Titles")
+    xtitle, ytitle = preparingTitles(param_dict)
+
+    gSave = []
+    j = 0
+
+    if isinstance(param_dict['data'],list):
+
+        if 'n_bins' in param_dict:
+            nbins = param_dict['n_bins']
+        else:
+            nbins = 100
+        list_histo = []
+
+        myfile = ROOT.TFile(param_dict['input_file_name'],"READ")
+        for namedata in param_dict['data']:
+            if not isinstance(namedata, list):
+                logger.critical(' {} is not a list of list; required for spectra ploting; skipping'.format(namedata))
+                continue
+            list_dataX = []
+            list_dataY = []
+            # myfile.Close()
+            tree = myfile.Get(param_dict['input_tree'])
+            n = tree.GetEntries()
+            for i in range(0,n):
+                tree.GetEntry(i)
+                list_dataX.append(getattr(tree,namedata[0]))
+                list_dataY.append(getattr(tree,namedata[1]))
+                # list_data.append(20*j+18510)
+
+            # list_histo.append()
+            if 'x_range' in param_dict:
+                if isinstance(param_dict['x_range'],list):
+
+                    if (isinstance(param_dict['x_range'][0],float) or isinstance(param_dict['x_range'][0],int)) and (isinstance(param_dict['x_range'][1],float) or isinstance(param_dict['x_range'][1],int)):
+                        if  param_dict['x_range'][0] < param_dict['x_range'][1]:
+                            xmin = param_dict['x_range'][0]
+                            xmax = param_dict['x_range'][1]
+                        else:
+                            xmin,xmax = autoRangeList(list_dataX)
+                    elif isinstance(param_dict['x_range'][0],float):
+                        xtemp,xmax = autoRangeList(list_dataX)
+                        xmin = param_dict['x_range'][0]
+                    elif isinstance(param_dict['x_range'][1],float):
+                        xmin,xtemp = autoRangeList(list_dataX)
+                        xmax = param_dict['x_range'][1]
+                    else:
+                        xmin,xmax = autoRangeList(list_dataX)
+                else:
+                    xmin,xmax = autoRangeList(list_dataX)
+            else:
+                xmin,xmax = autoRangeList(list_dataX)
+            list_histo.append(ROOT.TH1F(namedata[1]+'_vs_' + namedata[0],namedata[1]+'_vs_' + namedata[0],nbins,xmin,xmax))
+            for i in range(0,n):
+                list_histo[j].Fill(list_dataX[i],list_dataY[i])
+                # print(list_data[i])
+            can.cd()
+            if (j==0):
+                list_histo[j].Draw('hist')
+                list_histo[j].GetXaxis().SetTitle(xtitle);
+                list_histo[j].SetTitle(title)
+                if 'y_range' in param_dict and isinstance(param_dict['y_range'],list):
+                    if (isinstance(param_dict['y_range'][0],float) or isinstance(param_dict['y_range'][0],int)) and (isinstance(param_dict['y_range'][1],float) or isinstance(param_dict['y_range'][1],int)):
+                        if  param_dict['y_range'][0] < param_dict['y_range'][1]:
+                            list_histo[j].GetYaxis().SetRangeUser(param_dict['y_range'][0],param_dict['y_range'][1])
+                    elif isinstance(param_dict['y_range'][0],float):
+                        ytemp,ymax = autoRangeContent(list_histo[j])
+                        ymin = param_dict['y_range'][0]
+                        list_histo[j].GetYaxis().SetRangeUser(ymin,ymax)
+                    elif isinstance(param_dict['y_range'][1],float):
+                        ymin,ytemp = autoRangeContent(list_histo[j])
+                        ymax = param_dict['y_range'][1]
+                        list_histo[j].GetYaxis().SetRangeUser(ymin,ymax)
+            else:
+                list_histo[j].Draw("SAME")
+            # gSave.append(list_histo[j])
+            can.Update()
+
+
+            j=j+1
+    gSave.append(can)
+
+    # Setting the picture file name
+    if 'output_path' in param_dict:
+        path = param_dict['output_path']
+    else:
+        path = "./"
+    if path.endswith('/')==False:
+        path = path + '/'
+    if title!=' ':
+        figurefullpath = path+title+'_'
+    else:
+        figurefullpath = path
+    for namedata in param_dict['data']:
+        figurefullpath += namedata + '_'
+    if figurefullpath.endswith('_'):
+        figurefullpath = figurefullpath[:-1]
+    if 'output_format' in param_dict:
+        figurefullpath += '.' + param_dict['output_format']
+    else:
+        figurefullpath += '.pdf'
+    can.SaveAs(figurefullpath)
+    # raw_input('Press <ret> to end -> ')
+
+    return can
+
+
+
+
+
 def histo2D(param_dict):
     # Preparing the canvas
     title, width, height = preparingCanvas(param_dict)

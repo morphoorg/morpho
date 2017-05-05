5381e15793564f933e2c0ec4cdb0480410c22818
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-05-04 19:52:38 -0700

    Adding preprocessing/bootstrapping

--- morpho/__init__.py
+++ morpho/__init__.py
@@ -8,5 +8,6 @@ __version__ = pkg_resources.require("morpho")[0].version.split('-')[0]
 __commit__ = pkg_resources.require("morpho")[0].version.split('-')[-1]
 
 from . import plot
+from . import preprocessing
 from . import postprocessing
 from . import loader

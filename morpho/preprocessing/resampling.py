98aef3db542c059349ae9103fd1b0bf1ee456161
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-05-05 13:57:54 -0700

    Bootstrapping technique working.

--- morpho/preprocessing/resampling.py
+++ morpho/preprocessing/resampling.py
@@ -7,8 +7,11 @@ logger = logging.getLogger(__name__)
 
 import ROOT as root
 
-
 def bootstrapping(param_dict):
+    '''
+    Resample the content of a tree usng a bootstrap technique (some samples can be used twice).
+    '''
+
     logger.debug("Making bootstrapping")
     input_file_name = param_dict['input_file_name']
     input_tree = param_dict['input_tree']
@@ -25,27 +28,23 @@ def bootstrapping(param_dict):
         logger.critical("indentical input and output. filename: {}; tree: {}".format(input_file_name,input_tree))
         raise
 
-    logger.debug(input_file_name)
     file = root.TFile(input_file_name,"READ")
     tree = file.Get(input_tree)
-    print(tree)
     nEntries = tree.GetEntries()
-    logger.debug(nEntries)
-
-
+    logger.debug("Sampling {} points from {} in {}:{}".format(number_interation,nEntries,input_file_name,input_tree))
     newtree=tree.CloneTree(0)
+    newtree.SetName(output_tree)
+    root.gRandom.SetSeed()
     for i in range(number_interation):
         n = root.gRandom.Uniform()*nEntries
         tree.GetEntry(int(n))
         newtree.Fill()
-        logger.debug(int(n))
-    logger.debug(tree.GetEntries())
-    file.Close()
     if input_file_name is not output_file_name:
         g=root.TFile(output_file_name,"RECREATE")
     else:
         g = root.TFile(input_file_name,"UPDATE")
     g.cd()
-    newtree.SetName(output_tree)
     newtree.Write()
     g.Close()
+    file.Close()
+    logger.debug("Resampling complete; results saved in {}:{}".format(output_file_name,output_tree))

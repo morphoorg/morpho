7c495acf12e6f4805281a045cbaaffc043e7dcd1
Joe Johnston <joe.johnston203@gmail.com>
2017-08-05 11:43:27 -0400

    Simple 1D histograms displayed along the top and right of 2D a posteriori plots

--- morpho/plot/histo.py
+++ morpho/plot/histo.py
@@ -78,7 +78,7 @@ def histo(param_dict):
                             xmin = param_dict['x_range'][0]
                             xmax = param_dict['x_range'][1]
                         else:
-                            xmin,xmax = _autoRangeList(list_data)
+                            xmin,xmax = _autoRangeListx(list_data)
                     elif isinstance(param_dict['x_range'][0],float):
                         xtemp,xmax = _autoRangeList(list_data)
                         xmin = param_dict['x_range'][0]
@@ -366,50 +366,38 @@ def aposteriori_distribution(param_dict):
         nbins_y = param_dict['n_bins_y']
     else:
         nbins_y = 100
-    list_histo = []
 
+    # Populate a grid of histograms with the parameters
     myfile = ROOT.TFile(param_dict['input_file_name'],"READ")
+    
     if 'data' in param_dict:
         namedata = param_dict['data']
-    list_2Dhisto = _prepare_couples(namedata)
-    list_histo = []
-    for item in list_2Dhisto:
-        if not isinstance(item, list):
-            logger.critical(' {} is not a list of list; required for spectra ploting; skipping'.format(namedata))
-            return
-        list_dataX = []
-        list_dataY = []
-        # myfile.Close()
-        tree = myfile.Get(param_dict['input_tree'])
-        n = tree.GetEntries()
-        for i in range(0,n):
-            tree.GetEntry(i)
-            list_dataX.append(getattr(tree,item[0]))
-            list_dataY.append(getattr(tree,item[1]))
-        histo = _get2Dhisto(list_dataX, list_dataY, [nbins_x,nbins_y], [0,0], '{}_{}'.format(item[0],item[1]))
-        histo.SetTitle("")
-        histo.GetXaxis().SetTitle(item[0])
-        histo.GetYaxis().SetTitle(item[1])
-        list_histo.append(histo)
+        
 
-    Ndiv = len(namedata)-1
+    if 'root_plot_option' in param_dict:
+        draw_opt_2d = param_dict['root_plot_option']
+    else:
+        draw_opt_2d = 'contz'
+
+    name_grid, draw_opts_grid = _fill_variable_grid(namedata,
+                                                    draw_opt_2d)
+    intput_tree_name = param_dict['input_tree']
+    hist_grid = _fill_hist_grid(intput_tree_name, name_grid, myfile,
+                                nbins_x, nbins_y)
+
+    # Plot all histograms
     ROOT.gStyle.SetOptStat(0)
-    can.Divide(Ndiv,Ndiv)
-    ix = 0
-    iy = 1
-    for hist in list_histo:
-        ix = ix + 1
-        ican = (iy-1)*Ndiv + ix
-        can.cd(ican)
-        if 'root_plot_option' in param_dict:
-            hist.Draw(param_dict['root_plot_option'])
-        else:
-            hist.Draw('contz')
-        if (ix==iy):
-            iy = iy+1
-            ix = 0
-
-    # Setting the picture file name
+    rows = len(hist_grid)
+    cols = len(hist_grid[0])
+    can.Divide(cols,rows)
+    for r in range(rows):
+        for c in range(cols):
+            if(not hist_grid[r][c] is None):
+                ican = 1+r*cols+c
+                can.cd(ican)
+                hist_grid[r][c].Draw(draw_opts_grid[r][c])
+                
+    # Save the plot
     if 'output_path' in param_dict:
         path = param_dict['output_path']
     else:
@@ -551,7 +539,6 @@ def _get2Dhisto(list_dataX, list_dataY, nbins, ranges,histo_title):
             ymin,ymax = _autoRangeList(list_dataY)
     else:
         ymin,ymax = _autoRangeList(list_dataY)
-
     temphisto = ROOT.TH2F(histo_title,histo_title,nbins[0],xmin,xmax,nbins[1],ymin,ymax)
     if len(list_dataX)!=len(list_dataX):
         logger.critical("list of data does not have the same size. x: {}; y: {}".format(len(list_dataX),len(list_dataY)))
@@ -577,3 +564,86 @@ def _autoRangeContent(hist):
     xmin = min(list)*0.9
     xmax = max(list)*1.1  # need to be done
     return xmin, xmax
+
+def _fill_variable_grid(variable_names, draw_opt_2d):
+    """ 
+    pre: variable_name: variables to plot
+         draw_opt_2D: Draw option to be used for 2D plots
+    post: Returns a 2-tuple, where the first element is a grid of
+          lists of variable names and the sedon is a grid of
+          options that should be used to plot each list. 
+          The grid of variable names will contain list length 1
+          with a single variable when a 1D histogram should be
+          plotted, and a list length 2 for 2D histograms.
+          Positions that should not have a plot contain None
+    """
+    rows, cols = len(variable_names), len(variable_names)
+    name_grid = [[None]*cols for i in range(rows)]
+    draw_opts_grid = [[None]*cols for i in range(rows)]
+    for i in range(0,len(variable_names)):
+        for j in range(0,len(variable_names)):
+            if(i==0 and j<cols-1):
+                # First Row
+                name_grid[i][j] = [variable_names[j]]
+                draw_opts_grid[i][j] = "bar"
+            elif(j==cols-1 and i>0):
+                # Last Column
+                name_grid[i][j] = [variable_names[(i-2)%cols]]
+                draw_opts_grid[i][j] = "hbar"
+            elif(i>0 and i+(cols-j)<=cols+1):
+                name_grid[i][j] = [variable_names[(i-2)%cols], variable_names[j]]
+                draw_opts_grid[i][j] = draw_opt_2d
+    return (name_grid, draw_opts_grid)
+
+def _fill_variable_grid_corr_plot(variable_names):
+    """ 
+    pre: variable_name: variables to plot
+    post: returns a grid of length 2 lists. The list in
+          position [i,j] contains the ith variable name
+          then the jth variable name
+    """
+    rows, cols = len(variable_names), len(variable_names)
+    name_grid = [[None]*cols for i in range(rows)]
+    for i in range(0,variable_names):
+        for j in range(0,variable_names):
+            name_grid[i][j] = [variable_names[i],
+                               variable_names[j]]
+    return name_grid
+
+def _fill_hist_grid(input_tree, name_grid, myfile,
+                    nbins_x, nbins_y):
+    rows, cols = len(name_grid), len(name_grid[0])
+    hist_grid = [[None]*cols for i in range(rows)]
+    tree = myfile.Get(input_tree)
+    n = tree.GetEntries()
+    for r,row in enumerate(name_grid):
+        for c,names in enumerate(row):
+            if(not names is None and len(names)==2):
+                list_dataX = []
+                list_dataY = []
+                for i in range(0,n):
+                    tree.GetEntry(i)
+                    list_dataY.append(getattr(tree, names[0]))
+                    list_dataX.append(getattr(tree, names[1]))
+                histo = _get2Dhisto(list_dataX, list_dataY, [nbins_x,nbins_y],
+                                    [0,0], '{}_{}'.format(names[0],names[1]))
+                histo.SetTitle("")
+                histo.GetYaxis().SetTitle(names[0])
+                histo.GetXaxis().SetTitle(names[1])
+                hist_grid[r][c] = histo
+            elif(not names is None and len(names)==1):
+                list_data = []
+                for i in range(0,n):
+                    tree.GetEntry(i)
+                    list_data.append(getattr(tree, names[0]))
+                x_range = _autoRangeList(list_data)
+                histo = ROOT.TH1F("%s_%i_%i"%(names[0],r,c), names[0],
+                                  nbins_x, x_range[0], x_range[1])
+                for i in range(len(list_data)):
+                    histo.Fill(list_data[i])
+                histo.SetTitle("")
+                histo.GetXaxis().SetTitle(names[0])
+                hist_grid[r][c] = histo
+            else:
+                hist_grid[r][c] = None
+    return hist_grid

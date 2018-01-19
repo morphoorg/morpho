3de28aef36ded38046d168c698044ff197cd478f
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-01-19 14:38:32 -0800

    Testing with the requirements in the setup.py file

--- setup.py
+++ setup.py
@@ -14,6 +14,25 @@ except Exception as err:
     print(err)
     verstr = 'v0.0.0-???'
 
+on_rtd  os.environ.get("READTHEDOCS", None) == 'True'
+requirements = list()
+standard_requirements = list('matplotlib==1.5.1')
+    'colorlog', 
+    'PyYAML==3.11', 
+    'pyparsing>=2.1.5', 
+    'numpy==1.13.1',
+    'pystan==2.17.0.0', 
+    'dnspython==1.12.0', 
+    'pbr==0.10.8', 
+    'wsgiref==0.1.2', 
+    'cycler==0.10.0', 
+    'python-dateutil==2.5.3')
+if not on_rtd:
+    requirements.append(standard_requirements)
+    ...
+else:
+    requirements.append('better-apidoc')
+
 extras_require = {
     'h5': ['h5py<=2.6'],
 }
@@ -27,18 +46,6 @@ setup(
     version=verstr,
     packages=['morpho', 'morpho/loader','morpho/plot','morpho/preprocessing','morpho/postprocessing'],
     scripts=['bin/morpho'],
-    install_requires=[
-        'matplotlib==1.5.1', 
-        'colorlog', 
-        'PyYAML==3.11', 
-        'pyparsing>=2.1.5', 
-        'numpy==1.13.1',
-        'pystan==2.17.0.0', 
-        'dnspython==1.12.0', 
-        'pbr==0.10.8', 
-        'wsgiref==0.1.2', 
-        'cycler==0.10.0', 
-        'python-dateutil==2.5.3'
-    ],
+    install_requires=requirements,
     extras_require=extras_require
 )

3d2431bff2bb6f7199196b4d6a467af24b3f9a44
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-01-19 17:17:14 -0800

    missing better api

--- setup.py
+++ setup.py
@@ -20,18 +20,21 @@ requirements = []
 extras_require = {
     'h5': ['h5py<=2.6'],
     'core':['matplotlib==1.5.1','colorlog', 'PyYAML==3.11','pyparsing>=2.1.5','numpy==1.13.1','pystan==2.17.0.0','dnspython==1.12.0','pbr==0.10.8','wsgiref==0.1.2','cycler==0.10.0','python-dateutil==2.5.3'],
-    'doc': ['sphinx', 'sphinx_rtd_theme', 'sphinxcontrib-programoutput', 'better-apidoc']
+    'doc': ['sphinx', 'sphinx_rtd_theme', 'sphinxcontrib-programoutput']
 }
+
+if on_rtd:
+    requirements.append('better-apidoc')
+    requirements += extras_require['doc']
+else:
+    requirements = extras_require['core']
+
 everything = set()
 for deps in extras_require.values():
     everything.update(deps)
 extras_require['all'] = everything
 
-if on_rtd:
-    requirements = extras_require['doc']
-else:
-    requirements = extras_require['core']
-
+print(requirements)
 setup(
     name='morpho',
     version=verstr,

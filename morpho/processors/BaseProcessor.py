fed2ede06e60264342683d650e95f80c63d49d3b
taliaweiss <tweiss@mit.edu>
2018-02-01 17:25:38 -0500

    Allow BaseProcessor to add param to dictionary

--- morpho/processors/BaseProcessor.py
+++ morpho/processors/BaseProcessor.py
@@ -41,6 +41,23 @@ class BaseProcessor:
                 data = default
         return data
 
+    def add_dict_param(dictionary, key, value):
+        '''
+        This method checks if a key already exists in a dictionary,
+        and if not, it adds the key and its corresponding value to
+        the dictionary.
+
+        Could be changed to take as input a list of tuples (key, value),
+        so multiple parameters may be added at once.
+        '''
+        if key in dictionary:
+            key_err = "Cannot add key {} to dictionary. That key is taken.".format(key) 
+            logger.error(key_err)
+            raise
+        else:
+            dict.update({key:value})
+        return dictionary           
+    
     def Configure(self, params):
         '''
         This method will be called by nymph to configure the processor

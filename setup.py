5fd80d8ecc97c88850591de508ac8156ccb1dd63
Joe Johnston <joe.johnston203@gmail.com>
2018-02-03 10:40:28 -0500

    Remove morpho 2 files from the morpho 1 develop branch
    morpho 2 files were mistakenly added when merging documentation into
    develop. A patch was created with the command
    git diff fed2ede a1b1126 > mypatch.txt, then was applied with
    git apply -3 mypatch.txt.

--- setup.py
+++ setup.py
@@ -38,9 +38,8 @@ print(requirements)
 setup(
     name='morpho',
     version=verstr,
-    packages=['morpho', 'morpho/utilities', 'morpho/processors'],
-    # packages=['morpho', 'morpho/loader','morpho/plot','morpho/preprocessing','morpho/postprocessing'],
-    scripts=['bin/morpho','bin/BasicTest'],
+    packages=['morpho', 'morpho/loader','morpho/plot','morpho/preprocessing','morpho/postprocessing'],
+    scripts=['bin/morpho'],
     install_requires=requirements,
     extras_require=extras_require,
     url='http://www.github.com/project8/morpho',

7896aa711cd339a77385bdefd8fea0f622e1b6da
Joe Johnston <joe.johnston203@gmail.com>
2018-02-07 18:45:35 -0500

    Fix postprocessing/__init__.py formatting

--- morpho/postprocessing/__init__.py
+++ morpho/postprocessing/__init__.py
@@ -1,10 +1,10 @@
 """Perform preprocessing routines designed to run before Stan
 
 Modules:
-  - general_data_reducer: Transform any spectrum into
+  - general\_data\_reducer: Transform any spectrum into
         a histogram of binned data points
-  - stan_utility: Perform Stan diagnostic tests
-  - data_reducer: Transform a spectrum into a histogram
+  - stan\_utility: Perform Stan diagnostic tests
+  - data\_reducer: Transform a spectrum into a histogram
         of binned data points, for frequency, KE, or time
         spectra. (Soon deprecated by general_data_reducer).
 """

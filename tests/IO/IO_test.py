21d81e3693fec863ac07f31de2aaf7abc57205d4
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-02-27 11:34:12 -0800

    Writting and reading with CVS processor

--- tests/IO/IO_test.py
+++ tests/IO/IO_test.py
@@ -1,10 +1,17 @@
-from morpho.processors.IO import IOProcessor
+from morpho.processors.IO import IOCVSProcessor
 
 config = {
     "action": "write",
-    "filename": "myFile.cvs",
-    "variables": ["x"],
+    "filename": "myFile.txt",
+    "variables": ["x","y"],
     "format": "cvs"
+}
+
+reader_config = {
+    "action": "read",
+    "filename": "myFile.txt",
+    "variables": ['x'],
+    "format": "csv"
 }
 
 data = {
@@ -12,7 +19,10 @@ data = {
     "y": [4,5,6,7]
 }
 
-a = IOProcessor("myIOProcessor")
+a = IOCVSProcessor("writer")
+b = IOCVSProcessor("reader")
 a.Configure(config)
+b.Configure(reader_config)
 a.data = data
 a.Run()
+print(b.Run())

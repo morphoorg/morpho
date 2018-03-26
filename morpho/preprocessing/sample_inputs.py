d5efc66c832e7504ae98ee1b1887bc8ea0aea4f2
taliaweiss <tweiss@mit.edu>
2018-03-26 11:34:44 -0400

    Added prior options to sample_inputs preprocessor

--- morpho/preprocessing/sample_inputs.py
+++ morpho/preprocessing/sample_inputs.py
@@ -52,27 +52,59 @@ def sample_inputs(param_dict):
     # Create a ROOT tree
     out_tree = ROOT.TTree(param_dict['tree'], param_dict['tree'])
 
-    for sample_dict in param_dict['params_to_sample']:
-        name = sample_dict['name']
-        if sample_dict['prior_dist'] == 'normal':
+    try:
+        for sample_dict in param_dict['params_to_sample']:
+            name = sample_dict['name']
+            dist_error = 'Sampling for {} distribution is not yet implemented'.format(sample_dict['prior_dist'])
             tmp_sampled_val = array('f',[ 0 ])
             b = out_tree.Branch(name, tmp_sampled_val, name+'/F')
+
             random.seed()
-            rand = random.gauss(sample_dict['prior_params'][0], sample_dict['prior_params'][1])
+            if 'strict_lower' in sample_dict:
+                rand = sample_dict['strict_lower']-1
+                while rand < sample_dict['strict_lower']:
+                    rand = _draw_random_sample(sample_dict, dist_error)
+            elif 'strict_upper' in sample_dict:
+                try:
+                    if rand > sample_dict['strict_upper']:
+                        while rand < sample_dict['strict_lower'] or rand > sample_dict['strict_upper']:
+                            rand = _draw_random_sample(sample_dict, dist_error)
+                except:
+                    rand = sample_dict['strict_upper']+1
+                    while rand > sample_dict['strict_upper']:
+                        rand = _draw_random_sample(sample_dict, dist_error)
+            else:
+                rand = _draw_random_sample(sample_dict, dist_error)
+
             logger.info('Sampled value of {}: {}'.format(name, rand))
             tmp_sampled_val[0] = rand
             b.Fill()
+    except:
+        pass
 
-        else:
-            logger.debug('Sampling for {} distributions is not yet implemented'.format(sample_dict['prior_dist']))
-
-    for fixed_dict in param_dict['fixed_params']:
-        tmp_fixed_val = array('f',[ 0 ])
-        b = out_tree.Branch(fixed_dict['name'], tmp_fixed_val, fixed_dict['name']+'/F')
-        tmp_fixed_val[0] = fixed_dict['val']
-        b.Fill()
+    try:
+        for fixed_dict in param_dict['fixed_params']:
+            tmp_fixed_val = array('f',[ 0 ])
+            b = out_tree.Branch(fixed_dict['name'], tmp_fixed_val, fixed_dict['name']+'/F')
+            tmp_fixed_val[0] = fixed_dict['val']
+            b.Fill()
+    except:
+        pass
 
     out_tree.Fill()
     outfile.Write()               
     outfile.Close()
     logger.info("Input parameters have been selected. Params in file: {}".format(param_dict['output_file_name']))
+
+
+def _draw_random_sample(sample_dict, dist_error):
+    if sample_dict['prior_dist'] == 'normal':
+        rand = random.gauss(sample_dict['prior_params'][0], sample_dict['prior_params'][1])
+    elif sample_dict['prior_dist'] == 'lognormal':
+        rand = np.random.lognormal(sample_dict['prior_params'][0], sample_dict['prior_params'][1], 1)[0]
+    elif sample_dict['prior_dist'] == 'beta':
+        rand = np.random.beta(sample_dict['prior_params'][0], sample_dict['prior_params'][1], 1)[0]
+    else:
+        logger.debug(dist_error)
+        return None
+    return rand

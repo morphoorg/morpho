a0fa7fb273ac73e4552182f1d881bfcd2227edf2
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-03-26 19:36:09 -0700

    Adding yaml as child class of json. added unittest

--- morpho/processors/IO/__init__.py
+++ morpho/processors/IO/__init__.py
@@ -5,5 +5,6 @@ from __future__ import absolute_import
 
 from .IOProcessor import IOProcessor
 from .IOCVSProcessor import IOCVSProcessor
+from .IOJSONProcessor import IOJSONProcessor, IOYAMLProcessor
 from .IORProcessor import IORProcessor
 from .IOROOTProcessor import IOROOTProcessor

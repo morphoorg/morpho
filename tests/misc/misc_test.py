17f861f2b11d7cf68bb91c304197f3c8311b5f53
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-03-30 18:44:03 -0700

    Adding tests

---
+++ tests/misc/misc_test.py
@@ -0,0 +1,28 @@
+'''
+This scripts aims at testing miscellaneous processors.
+Author: M. Guigue
+Date: Mar 30 2018
+'''
+
+import unittest
+
+from morpho.utilities import morphologging
+logger = morphologging.getLogger(__name__)
+
+class MiscTests(unittest.TestCase):
+
+    def test_ProcAssistant(self):
+        from morpho.processors.misc import ProcessorAssistant
+        proc_config = {
+            "function_name": "myFunction",
+            "module_name": "myModule",
+            "value": 10
+        }
+        assistantProcessor = ProcessorAssistant("assistantProcessor")
+        assistantProcessor.Configure(proc_config)
+        result = assistantProcessor.Run()
+        logger.debug("Assistant processor returned: {}".format(result))
+        self.assertEqual(result,"value=10")
+
+if __name__ == '__main__':
+    unittest.main()

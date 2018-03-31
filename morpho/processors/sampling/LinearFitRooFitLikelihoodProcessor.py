1d6bfc4964a1c0d0e61c53f8e1a1a0f5c604ed79
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-03-30 17:40:53 -0700

    Update LinearFit: not working. Need to change the _defineDataset to create a dataset containing simultaneously x and y values...

--- morpho/processors/sampling/LinearFitRooFitLikelihoodProcessor.py
+++ morpho/processors/sampling/LinearFitRooFitLikelihoodProcessor.py
@@ -1,21 +1,41 @@
 from morpho.utilities import morphologging, reader
 from morpho.processors.sampling import RooFitLikelihoodSampler
-# from RooFitLikelihoodSampler import RooFitLikelihoodSampler
 logger = morphologging.getLogger(__name__)
-
-import Constants
 
 import ROOT
 
-class LinearFitRooFitLikelihoodSampler(RooFitLikelihoodSampler):
+class LinearFitRooFitLikelihoodProcessor(RooFitLikelihoodSampler):
+    '''
+    Linear fit of data using RootFit Likelihood sampler.
+
+    '''
+
+    def Configure(self,config_dict = {}):
+        super().Configure(config_dict)
+        self.a_min, self.a_max = reader.read_param(config_dict,"paramRange", "required")["a"]
+        self.b_min, self.b_max = reader.read_param(config_dict,"paramRange", "required")["b"]
+        self.x_min, self.x_max = reader.read_param(config_dict,"paramRange", "required")["x"]
+        self.y_min, self.y_max = reader.read_param(config_dict,"paramRange", "required")["y"]
 
     def definePdf(self,wspace):
         '''
-        Defines the Pdf that RooFit will sample and add it to the workspace.
-        The Workspace is then returned by the user.
-        Users should edit this function.
+        
         '''
         logger.debug("Defining pdf")
-        var = wspace.var(self.varName)
+        # x = ROOT.RooRealVar("x","x")
+        # y = ROOT.RooRealVar("y","y")
+        a = ROOT.RooRealVar("a","a",self.a_min,self.a_max)
+        b = ROOT.RooRealVar("b","b",self.b_min,self.b_max)
+        x = ROOT.RooRealVar("x","x",self.x_min,self.x_max)
+        y = ROOT.RooRealVar("y","y",self.y_min,self.y_max)
+        width = ROOT.RooRealVar("width","width",0,1000)
+        # b = ROOT.RooRealVar("b","b")
+        residual = ROOT.RooFormulaVar("res","y-a*x-b", ROOT.RooArgList(x,y,a,b))
+        null = ROOT.RooRealVar("null","null",0.)
+        pdf = ROOT.RooGaussian("pdf","pdf",residual,null,width)
+
+
+        # Save pdf: this will save all required variables and functions
+        getattr(wspace,'import')(pdf)
 
         return wspace
--- morpho/processors/sampling/__init__.py
+++ morpho/processors/sampling/__init__.py
@@ -6,3 +6,4 @@ from __future__ import absolute_import
 from .GaussianSamplingProcessor import GaussianSamplingProcessor
 from .PyStanSamplingProcessor import PyStanSamplingProcessor
 from .RooFitLikelihoodSampler import RooFitLikelihoodSampler
+from .LinearFitRooFitLikelihoodProcessor import LinearFitRooFitLikelihoodProcessor

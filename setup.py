86d103aee5dae8014df15132176066d9a795809b
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-04-05 18:57:26 -0700

    matplotlib has stronger preferences for dateutils

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ on_rtd = os.environ.get("READTHEDOCS", None) == 'True'
 requirements = []
 extras_require = {
     'h5': ['h5py<=2.6'],
-    'core':['uproot>=2.8.13','matplotlib==1.5.1','colorlog', 'PyYAML==3.11','pyparsing>=2.1.5','numpy>=1.14','pystan==2.17.0.0','dnspython==1.12.0','pbr==0.10.8','cycler==0.10.0','python-dateutil==2.5.3'],
+    'core':['uproot>=2.8.13','matplotlib==1.5.1','colorlog', 'PyYAML==3.11','pyparsing>=2.1.5','numpy>=1.14','pystan==2.17.0.0','dnspython==1.12.0','pbr==0.10.8','cycler==0.10.0'],
     'doc': ['sphinx', 'sphinx_rtd_theme', 'sphinxcontrib-programoutput']
 }
 

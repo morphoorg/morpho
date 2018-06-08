3c21aaea69dd4f716381cc0bc9d5c290faa2c502
Mathieu Guigue <mathieu.guigue@pnnl.gov>
2018-06-07 23:41:53 -0700

    Cleaning up

--- morpho/__init__.py
+++ morpho/__init__.py
@@ -13,7 +13,5 @@ import pkg_resources
 __version__ = pkg_resources.require("morpho")[0].version.split('-')[0]
 __commit__ = pkg_resources.require("morpho")[0].version.split('-')[-1]
 
-from . import plot
-from . import preprocessing
-from . import postprocessing
-from . import loader
+from . import processors
+from . import utilities
--- morpho/processors/__init__.py
+++ morpho/processors/__init__.py
@@ -4,5 +4,8 @@
 from __future__ import absolute_import
 
 from .BaseProcessor import BaseProcessor
-from . import sampling
 from . import diagnostics
+from . import IO
+from . import misc
+from . import plots
+from . import sampling

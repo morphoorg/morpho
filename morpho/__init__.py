0fdd16f1171680c798ad89354ec01a756c1025c5
Mathieu Guigue <mathieu.guigue@pnnl.gov>
2018-06-17 10:16:51 -0700

    Modifying the morpho.__init__: I can import BaseProcessor using `from morpho import X`

--- morpho/__init__.py
+++ morpho/__init__.py
@@ -13,5 +13,18 @@ import pkg_resources
 __version__ = pkg_resources.require("morpho")[0].version.split('-')[0]
 __commit__ = pkg_resources.require("morpho")[0].version.split('-')[-1]
 
-from . import processors
-from . import utilities
+# from . import processors
+# from . import utilities
+
+__all__ = []
+import pkgutil
+import inspect
+
+for loader, name, is_pkg in pkgutil.walk_packages(__path__):
+    module = loader.find_module(name).load_module(name)
+    for name, value in inspect.getmembers(module):
+        if name.startswith("__"):
+          continue
+        
+        globals()[name] = value
+        __all__.append(name)

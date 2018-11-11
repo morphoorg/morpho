acb66a6f75fb905fa209bfcdee913cada03be39e
Mathieu GUIGUE <mgguigue@gmail.com>
2018-11-11 15:37:27 +0100

    Starting new example

---
+++ examples/gaussian_model/scripts/gaus_sol.py
@@ -0,0 +1,8 @@
+# Basic gaussian data generator (solution)
+# This script will generate fake data following a gaussian distribution.
+# Author: M. Guigue
+# Date: 11/11/2018
+
+from morpho.utilities import morphologging
+
+logger = logger = morphologging.getLogger(__name__)

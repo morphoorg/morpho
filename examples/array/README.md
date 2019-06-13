4ee39dda8c0484bbfa3cdd1015a20c2ca7bbcb25
Mathieu GUIGUE <mgguigue@gmail.com>
2019-06-13 11:28:37 +0200

    Updating documentation

--- examples/array/README.md
+++ examples/array/README.md
@@ -1,3 +1,4 @@
-This folder contains an exercise (and its solution) for understanding how to work with the python API of morpho.
-The idea is to generate fake data following a Gaussian distribution and to plot the data into an histogram.
-The script "gaus_ex.py" needs to be filled with the missing statements, the answers can be found in "gaus_sol.py".
+# Array example
+
+This folder contains an example of a Stan model that inputs arrays to generate vector data.
+The data are then saved into a R file.
--- examples/array/scripts/pystan_test.py
+++ examples/array/scripts/pystan_test.py
@@ -1,6 +1,6 @@
-# Basic linear fit example
-# This config file can be used to generate and analyze data following an
-# linear function. Control plots are then generated.
+# Basic array generator
+# This config file can be used to generate list of parameters that have a gaussian
+# distribution with means and widths given by the config file.
 # Note that each processor are created, configured, run and connected manually
 # Author: M. Guigue
 
@@ -11,8 +11,8 @@ from morpho.processors.IO import IORProcessor
 generator_config = {
     "model_code": "models/gaussian_gen.stan",
     "input_data": {
-        "means": [1,3,9],
-        "widths": [0.5,1,0.5]
+        "means": [1, 3, 9],
+        "widths": [0.5, 1, 0.5]
     },
     "iter": 3000,
     "warmup": 500,
@@ -23,49 +23,15 @@ generator_config = {
 writer_config = {
     "action": "write",
     "filename": "data/data.r",
-    "variables": ["x[0]"]
+    "variables": ["x[0]", "x[1]"] # here we are saving only the first 2 variables of x
 }
-# reader_config = {
-#     "action": "read",
-#     "filename": "linear_fit/data/data.r",
-#     "variables": ["x", "y"]
-# }
-# analyzer_config = {
-#     "model_code": "linear_fit/models/model_linear_fit.stan",
-#     "iter": 2500,
-#     "warmup": 500,
-#     "interestParams": ['slope', 'intercept', 'sigma'],
-#     "diagnostics_folder": "linear_fit/plots/analyzer_diagnostics"
-# }
-# aposteriori_config = {
-#     "n_bins_x": 100,
-#     "n_bins_y": 100,
-#     "variables": ['slope', 'intercept', 'sigma', "lp_prob"],
-#     "title": "aposteriori_distribution",
-#     "output_path": "linear_fit/plots"
-# }
-# timeSeries_config = {
-#     "variables": ['slope', 'intercept', 'sigma'],
-#     "height": 1200,
-#     "title": "timeseries",
-#     "output_path": "linear_fit/plots"
-# }
 
 # Definition of the processors
 generationProcessor = PyStanSamplingProcessor("generator")
 writerProcessor = IORProcessor("writer")
-# readerProcessor = IORProcessor("reader")
-# analysisProcessor = PyStanSamplingProcessor("analyzer")
-# aposterioriPlotter = APosterioriDistribution("posterioriDistrib")
-# timeSeriesPlotter = TimeSeries("timeSeries")
-
 # Configuration step
 generationProcessor.Configure(generator_config)
 writerProcessor.Configure(writer_config)
-# readerProcessor.Configure(reader_config)
-# analysisProcessor.Configure(analyzer_config)
-# aposterioriPlotter.Configure(aposteriori_config)
-# timeSeriesPlotter.Configure(timeSeries_config)
 
 # Run step
 # Generate datapoints
@@ -73,15 +39,3 @@ generationProcessor.Run()
 writerProcessor.data = generationProcessor.results
 # Save data points
 writerProcessor.Run()
-# Read data points
-# readerProcessor.Run()
-# analysisProcessor.data = readerProcessor.data
-# analysisProcessor.data = {'N': len(analysisProcessor.data['x'])}
-# # Run analysis on data points
-# analysisProcessor.Run()
-# results = analysisProcessor.results
-# aposterioriPlotter.data = results
-# timeSeriesPlotter.data = results
-# # Plot analysis results
-# aposterioriPlotter.Run()
-# timeSeriesPlotter.Run()

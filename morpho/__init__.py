7604e225ef445692625db909ee2681d2290f82b2
Mathieu GUIGUE <mgguigue@gmail.com>
2022-01-01 23:50:46 +0100

    Remove pkg resources (depreciated) and UT
    Switch to Github CI system

--- morpho/__init__.py
+++ morpho/__init__.py
@@ -9,9 +9,9 @@ Subpackages:
 
 from __future__ import absolute_import
 
-import pkg_resources
-__version__ = pkg_resources.require("morpho")[0].version.split('-')[0]
-__commit__ = pkg_resources.require("morpho")[0].version.split('-')[-1]
+# import pkg_resources
+# __version__ = pkg_resources.require("morpho")[0].version.split('-')[0]
+# __commit__ = pkg_resources.require("morpho")[0].version.split('-')[-1]
 
 # from . import processors
 # from . import utilities
